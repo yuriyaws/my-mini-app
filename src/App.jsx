10a0abc098927bb89e6a5fda50a6dcb785130144
Admin <admin@MacBook-Air-Admin.local>
2025-06-12 20:43:49 +0300

    Added useEffect to get telegram userId

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
-import './App.css'
-import Home from './home'
-import Add from './add'
-import Cart from './cart'
-import Orders from './orders'
-import Adress from './adress'
-import { useState } from 'react'
-
+import "./App.css";
+import Home from "./home";
+import Add from "./add";
+import Cart from "./cart";
+import Orders from "./orders";
+import Adress from "./adress";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [page, setPage] = useState('home')
+  const [page, setPage] = useState("home");
+
+  const [tgUserId, setTgUserId] = useState(null);
+
+  useEffect(() => {
+    if (window.Telegram && window.Telegram.WebApp) {
+      const user = window.Telegram.WebApp.initDataUnsafe?.user;
+      if (user) {
+        setTgUserId(user.id);
+        console.log("Telegram user ID:", user.id);
+      }
+    }
+  }, []);
 
   return (
     <>
-      <div className='fixed bottom-0 bg-white border-t border-gray-300 w-full h-14 z-10 flex flex-row justify-between px-10'>
+      <div className="fixed bottom-0 bg-white border-t border-gray-300 w-full h-14 z-10 flex flex-row justify-between px-10">
         <button
-          className='flex flex-col justify-center items-center'
-          onClick={() => setPage('home')}
+          className="flex flex-col justify-center items-center"
+          onClick={() => setPage("home")}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -23,16 +34,24 @@ function App() {
             viewBox="0 0 24 24"
             strokeWidth="1.5"
             stroke="currentColor"
-            className={`size-5 ${page === 'home' ? 'text-blue-600' : 'text-gray-500'}`}
+            className={`size-5 ${page === "home" ? "text-blue-600" : "text-gray-500"}`}
           >
-            <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+            />
           </svg>
-          <p className={`text-xs ${page === 'home' ? 'text-blue-600' : 'text-gray-500'}`}>Главная</p>
+          <p
+            className={`text-xs ${page === "home" ? "text-blue-600" : "text-gray-500"}`}
+          >
+            Главная
+          </p>
         </button>
 
         <button
-          className='flex flex-col justify-center items-center'
-          onClick={() => setPage('add')}
+          className="flex flex-col justify-center items-center"
+          onClick={() => setPage("add")}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -40,33 +59,50 @@ function App() {
             viewBox="0 0 24 24"
             strokeWidth="1.5"
             stroke="currentColor"
-            className={`size-5 ${page === 'add' ? 'text-blue-600' : 'text-gray-500'}`}
+            className={`size-5 ${page === "add" ? "text-blue-600" : "text-gray-500"}`}
           >
-            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 4.5v15m7.5-7.5h-15"
+            />
           </svg>
-          <p className={`text-xs ${page === 'add' ? 'text-blue-600' : 'text-gray-500'}`}>Добавить</p>
+          <p
+            className={`text-xs ${page === "add" ? "text-blue-600" : "text-gray-500"}`}
+          >
+            Добавить
+          </p>
         </button>
 
         {/* Остальные пункты можно сделать аналогично */}
-        <button className='flex flex-col justify-center items-center'
-          onClick={() => setPage('cart')}
+        <button
+          className="flex flex-col justify-center items-center"
+          onClick={() => setPage("cart")}
         >
-        
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
             strokeWidth="1.5"
             stroke="currentColor"
-            className={`size-5 ${page === 'cart' ? 'text-blue-600' : 'text-gray-500'}`}
+            className={`size-5 ${page === "cart" ? "text-blue-600" : "text-gray-500"}`}
           >
-            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />       
-         </svg>
-          <p className={`text-xs ${page === 'cart' ? 'text-blue-600' : 'text-gray-500'}`}>Корзина</p>
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+            />
+          </svg>
+          <p
+            className={`text-xs ${page === "cart" ? "text-blue-600" : "text-gray-500"}`}
+          >
+            Корзина
+          </p>
         </button>
 
-        <button className='flex flex-col justify-center items-center'
-        onClick={()=> setPage('orders')}
+        <button
+          className="flex flex-col justify-center items-center"
+          onClick={() => setPage("orders")}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -74,22 +110,30 @@ function App() {
             viewBox="0 0 24 24"
             strokeWidth="1.5"
             stroke="currentColor"
-            className={`size-5 ${page === 'orders' ? 'text-blue-600' : 'text-gray-500'}`}
+            className={`size-5 ${page === "orders" ? "text-blue-600" : "text-gray-500"}`}
           >
-            <path stroke-linecap="round" stroke-linejoin="round" d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184" />
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184"
+            />
           </svg>
-          <p className={`text-xs ${page === 'orders' ? 'text-blue-600' : 'text-gray-500'}`}>Заказы</p>
+          <p
+            className={`text-xs ${page === "orders" ? "text-blue-600" : "text-gray-500"}`}
+          >
+            Заказы
+          </p>
         </button>
       </div>
 
       {/* Контент */}
-      {page === 'home' && <Home setPage={setPage} />}
-      {page === 'add' && <Add />}
-      {page === 'cart' && <Cart setPage={setPage} />}
-      {page === 'orders' && <Orders />}
-      {page === 'adress' && <Adress setPage={setPage}/>}
+      {page === "home" && <Home setPage={setPage} />}
+      {page === "add" && <Add />}
+      {page === "cart" && <Cart setPage={setPage} />}
+      {page === "orders" && <Orders />}
+      {page === "adress" && <Adress setPage={setPage} />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
