6afe7752dfd1dc101454281311e878f63ba05f3d
Admin <admin@MacBook-Air-Admin.local>
2025-06-15 03:06:51 +0300

    final 1

--- src/orders.jsx
+++ src/orders.jsx
@@ -1,33 +1,43 @@
-import { useState } from 'react'
-import OrdersActive from './ordersActive'
-import OrdersComplete from './ordersComplete'
+import { useState } from "react";
+import OrdersActive from "./ordersActive";
+import OrdersComplete from "./ordersComplete";
 
-function Orders() {
-    const [page, setPage] = useState('ordersActive')
+function Orders({ tgUserId }) {
+  const [page, setPage] = useState("ordersActive");
 
-   return(
-        <>
-        <div className="px-5 mt-7 mb-3">
+  return (
+    <>
+      <div className="px-5 mt-7 mb-3">
         <h1 className="text-xl font-bold text-black">Заказы</h1>
-            <div className="mt-3 bg-gray-100 rounded-lg px-2 py-1 flex flex-row gap-3">
-                <button className={page === 'ordersActive' ? 'w-full py-1 bg-white rounded-lg text-sm' : 'w-full py-1 text-sm'}
-                onClick={() => setPage('ordersActive')}
-                >
-                    Активные
-                </button>
-                <button className={page === 'ordersComplete' ? 'w-full py-1 bg-white rounded-lg text-sm' : 'w-full py-1 text-sm'}
-                onClick={() => setPage('ordersComplete')}
-                >
-                    Завершенные
-                </button>
-            </div>
+        <div className="mt-3 bg-gray-100 rounded-lg px-2 py-1 flex flex-row gap-3">
+          <button
+            className={
+              page === "ordersActive"
+                ? "w-full py-1 bg-white rounded-lg text-sm"
+                : "w-full py-1 text-sm"
+            }
+            onClick={() => setPage("ordersActive")}
+          >
+            Активные
+          </button>
+          <button
+            className={
+              page === "ordersComplete"
+                ? "w-full py-1 bg-white rounded-lg text-sm"
+                : "w-full py-1 text-sm"
+            }
+            onClick={() => setPage("ordersComplete")}
+          >
+            Завершенные
+          </button>
         </div>
+      </div>
 
-        {/* Контент */}
-        {page === 'ordersActive' && <OrdersActive />}
-        {page === 'ordersComplete' && <OrdersComplete />}
-        </>
-    )
+      {/* Контент */}
+      {page === "ordersActive" && <OrdersActive tgUserId={tgUserId} />}
+      {page === "ordersComplete" && <OrdersComplete tgUserId={tgUserId} />}
+    </>
+  );
 }
 
-export default Orders
+export default Orders;
