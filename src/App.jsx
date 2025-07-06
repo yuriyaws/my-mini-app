73f146c9c8faca2dfef884b647b6cab149468138
Admin <admin@MacBook-Air-Admin.local>
2025-07-06 13:01:41 +0300

    Добавил проверку на админа. Показывать панель или нет

--- src/App.jsx
+++ src/App.jsx
@@ -153,31 +153,33 @@ function App() {
           </p>
         </button>
 
-        <button
-          className="flex flex-col justify-center items-center"
-          onClick={() => setPage("admin")}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            className={`size-5 ${page === "admin" ? "text-blue-600" : "text-gray-500"}`}
+        {tgUserId === 1919233418 ? (
+          <button
+            className="flex flex-col justify-center items-center"
+            onClick={() => setPage("admin")}
           >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
-            />
-          </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className={`size-5 ${page === "admin" ? "text-blue-600" : "text-gray-500"}`}
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+              />
+            </svg>
 
-          <p
-            className={`text-xs ${page === "admin" ? "text-blue-600" : "text-gray-500"}`}
-          >
-            Админ
-          </p>
-        </button>
+            <p
+              className={`text-xs ${page === "admin" ? "text-blue-600" : "text-gray-500"}`}
+            >
+              Админ
+            </p>
+          </button>
+        ) : null}
       </div>
 
       {/* Контент */}
